4aee1ab893fd440476bb00b97cc0e444c4077d33
Abhishek Choudhary <135415468+iamabhishek2828@users.noreply.github.com>
2024-11-16 11:24:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,18 @@ access_token = google_credentials.token
 
 # Initialize Firebase Admin SDK only once
 if not firebase_admin._apps:
-    firebase_credentials = credentials.Certificate(firebase_credentials_path)
+    firebase_credentials = credentials.Certificate({
+        "type": st.secrets["firebase"]["type"],
+        "project_id": st.secrets["firebase"]["project_id"],
+        "private_key_id": st.secrets["firebase"]["private_key_id"],
+        "private_key": st.secrets["firebase"]["private_key"],
+        "client_email": st.secrets["firebase"]["client_email"],
+        "client_id": st.secrets["firebase"]["client_id"],
+        "auth_uri": st.secrets["firebase"]["auth_uri"],
+        "token_uri": st.secrets["firebase"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"],
+    })
     firebase_admin.initialize_app(firebase_credentials)
 else:
     st.warning("Firebase app is already initialized.")
